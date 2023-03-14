9988271d391376d6dd4c16a567dca16747681ba2
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2023-03-13 22:17:47 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	const setHeight = () => {
+		const mainEl = document.getElementById('root-container');
+		if (mainEl) {
+			mainEl.style.minHeight = window.innerHeight + 'px';
+		}
+	};
+
+	onMount(() => {
+		setHeight();
+	});
+</script>
+
+<svelte:window on:resize={setHeight} />
+
+<svelte:head>
+	<title>Lucas Neuhaus</title>
+</svelte:head>
+
+<main class="min-v-screen min-h-screen bg-blue-50">
+	<slot />
+</main>
